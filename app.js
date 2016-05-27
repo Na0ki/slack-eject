660501c5e026db92d873a65b1cf4f47996668e1b
Na0ki <wellingtonnmaeda0524@gmail.com>
2016-05-27 20:53:49 +0900

    miscellaneous commit

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+'use strict';
+
 var Botkit = require('botkit');
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
+var Promise = require('bluebird');
 var config = require('./config');
 
 var controller = Botkit.slackbot({
@@ -15,25 +18,117 @@ controller.spawn({
 
 
 // eject実行
-controller.hears(['eject (.*)'], ['message_received', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
-    var deviceType = message.match[1]; // (.*) を取得
+controller.hears(['^eject (.*)$'], ['message_received', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    var deviceType = message.text.match[1];
+    console.log("deviceType: ", deviceType);
 
-    // CD-ROMドライブ以外をejectできないようにする
-    if (!deviceType.match(/cdrom/)) {
-        return bot.reply(message, '[Error] ( ՞ةڼ◔)oO( デバイスタイプが不正でし )');
+    if (!deviceType.match(/\/dev\//)) {
+        bot.reply(message, "Usage: eject /dev/foo");
+        return;
     }
 
     // ejectコマンド実行
     exec('eject -T ' + deviceType, function (err, stdout) {
-
+        if (err) {
+            console.error(err);
+            bot.reply(message, "Error Occurred on Eject");
+        }
+        console.log(stdout);
         var cmd = 'traystatus ' + deviceType + ' && echo 1';
 
-        function shSpawn(command) {
-            return spawn('sh', ['-c', command]);
+        execCmd(cmd)
+            .then(function (res, err) {
+                var replyMsg = "";
+                console.log("buffer: " + res);
+                if (res.match(/1/)) {
+                    replyMsg = "開いてる";
+                } else {
+                    replyMsg = "閉まってる";
+                }
+
+                bot.reply(message, genMsg(err, replyMsg));
+            })
+            .catch(function (err) {
+                console.log(err);
+                var replyMsg = "[Error] " + err;
+
+                bot.reply(message, replyMsg);
+            });
+    });
+});
+
+
+// CD-ROMドライブの検索
+controller.hears(['^devices (.*)$'], ['message_received', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
+    var grepWord = message.match[1];
+    var cmd = "ls -l /dev/ | grep " + grepWord;
+
+    execCmd(cmd).then(function (res, err) {
+        if (err) {
+            console.error(err);
+            bot.reply(message, "Error Occurred on Eject");
+            return;
         }
 
+        var replyMsg = genMsg(undefined, res);
+        if (res.length == 0) {
+            replyMsg = "( ՞ةڼ◔)oO( CD-ROMドライブが見つからないぽよ )";
+        }
+
+        bot.reply(message, replyMsg);
+    });
+});
+
+
+//CD-ROMドライブのトレイの開閉状態を確認
+controller.hears(['status (.*)', 'Status (.*)'], ['message_received', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
+
+    var deviceType = message.match[1]; // (.*) を取得
+    if (!deviceType.match(/\/dev\//)) {
+        bot.reply(message, "Usage: status /dev/hoge");
+        return;
+    }
+
+    /**
+     * https://www.linuxquestions.org/questions/slackware-14/detect-cd-tray-status-4175450610/
+     * を参考にしてrpi内で作成したtraystatusコマンドを実行する
+     */
+    var cmd = 'traystatus ' + deviceType + ' && echo 1';
+
+    execCmd(cmd)
+        .then(function (res, err) {
+            var replyMsg = "";
+            if (err) {
+                replyMsg = "[Error] " + err;
+                console.log(replyMsg);
+                bot.reply(message, replyMsg);
+                return;
+            }
+            console.log("buffer: " + res);
+            if (res.match(/1/)) {
+                replyMsg = "開いてる";
+            } else {
+                replyMsg = "閉まってる";
+            }
+            bot.reply(message, replyMsg);
+        })
+        .catch(function (err) {
+            console.log(err);
+            var replyMsg = "[Error] " + err;
+            bot.reply(message, replyMsg);
+        });
+});
+
+
+/**
+ *
+ * @param cmd 実行コマンド
+ * @returns {Promise}
+ */
+function execCmd(cmd) {
+    return new Promise(function (resolve, reject) {
         var child = shSpawn(cmd);
-        var buf = "";
+        var buf = ""; // 実行結果を格納するバッファ
 
         child.stdout.on('data', function (data) {
             buf = buf + data;
@@ -41,97 +136,32 @@ controller.hears(['eject (.*)'], ['message_received', 'direct_message', 'direct_
 
         child.stderr.on('data', function (data) {
             console.log('exec error: ' + data);
+            reject(data);
         });
 
         child.on('close', function (code) {
-            var replyMsg = "";
-            console.log("buffer: " + buf);
-            if (buf.match(/1/)) {
-                replyMsg = "開いてる";
-            } else {
-                replyMsg = "閉まってる";
-            }
-            bot.reply(message, genMsg(err, replyMsg));
+            console.info(code);
+            resolve(buf);
         });
     });
-});
+}
 
-
-// cdromデバイス検索
-controller.hears(['devices'], ['message_received', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
-    var cmd = "ls -l /dev/ | grep cdrom";
-
-    function shSpawn(command) {
-        return spawn('sh', ['-c', command]);
-    }
-
-    var child = shSpawn(cmd);
-    var buf = "";
-
-    child.stdout.on('data', function (data) {
-        buf = buf + data;
-    });
-
-    child.stderr.on('data', function (data) {
-        console.log('exec error: ' + data);
-    });
-
-    child.on('close', function (code) {
-        var replyMsg = genMsg(undefined, buf);
-        if (buf.length == 0) {
-            replyMsg = "( ՞ةڼ◔)oO( CD-ROMドライブが見つからないぽよ )";
-        }
-        bot.reply(message, replyMsg);
-    });
-});
-
-
-// cdromの開閉状態を確認
-controller.hears(['status (.*)'], ['message_received', 'direct_message', 'direct_mention', 'mention'], function (bot, message) {
-
-    var deviceType = message.match[1]; // (.*) を取得
-    
-    /**
-     * https://www.linuxquestions.org/questions/slackware-14/detect-cd-tray-status-4175450610/
-     * を参考にしてrpi内で作成したtraystatusコマンドを実行する
-     */
-    var cmd = 'traystatus ' + deviceType + ' && echo 1';
-
-    function shSpawn(command) {
-        return spawn('sh', ['-c', command]);
-    }
-
-    var child = shSpawn(cmd);
-    var buf = "";
-
-    child.stdout.on('data', function (data) {
-        buf = buf + data;
-    });
-
-    child.stderr.on('data', function (data) {
-        console.log('exec error: ' + data);
-    });
-
-    child.on('close', function (code) {
-        var replyMsg = "";
-        console.log("buffer: " + buf);
-        if (buf.match(/1/)) {
-            replyMsg = "開いてる";
-        } else {
-            replyMsg = "閉まってる";
-        }
-        bot.reply(message, replyMsg);
-    });
-});
+/**
+ * コマンドを実行する
+ * @param command
+ */
+function shSpawn(command) {
+    return spawn('sh', ['-c', command]);
+}
 
 
 // リプライメッセージ生成
-function genMsg(err, stdout) {
+function genMsg(err, msg) {
     var replyMsg = "";
     if (err) {
         replyMsg = '[Error]\n( ՞ةڼ◔)oO(\n' + err + '\n)';
     } else {
-	var message = (stdout.length == 0) ? "uiiin" : stdout;
+        var message = (msg.length == 0) ? "" : msg;
         replyMsg = '[Success]\n( ՞ਊ ՞)oO( ' + message + ' )';
     }
     return replyMsg;
